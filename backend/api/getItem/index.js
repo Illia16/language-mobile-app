8000f80c4aa375af1c76a1ddfb5d66b597b65c3b
Illia16 <illia2143@gmail.com>
2022-01-27 20:36:58 -0500

    wip: backend

--- backend/api/getItem/index.js
+++ backend/api/getItem/index.js
@@ -1,57 +1,32 @@
-const AWS = require('aws-sdk')
-const dynamoDB = new AWS.DynamoDB({ region: 'us-east-1', apiVersion: '2012-08-10' })
+const AWS = require("aws-sdk");
+const dynamoDB = new AWS.DynamoDB({
+  region: "us-east-1",
+  apiVersion: "2012-08-10",
+});
 
-exports.handler = (event, context, callback) => {
-    const getData = (params) => {
-        dynamoDB.scan(params, (err, data) => {
-          if (err) {
-              callback(err)
-          } else {
-                const unmarshalledData = data.Items.map(el => {
-                    return AWS.DynamoDB.Converter.unmarshall(el)
-                })
-
-                const response = {
-                    "statusCode": 200,
-                    "body": JSON.stringify(unmarshalledData),
-                    "headers": {
-                        "Access-Control-Allow-Origin": "*",
-    	            "Access-Control-Allow-Credentials": true
-                    }
-
-                };
-
-                callback(null, response)
-          }
-        })
-    }
-
-    const params = {
-        TableName: 'languageAppIn'
-    }
-
-    if (event.httpMethod === 'GET') {
-            getData(params)
-    }
-
-    if (event.httpMethod === 'POST') {
-        const parsedBody = JSON.parse(event.body);
-
-        dynamoDB.putItem({
-            TableName: "languageAppIn",
-            Item : {
-                "time": {N: parsedBody.time},
-                "moves": {N: parsedBody.moves},
-                "name": {S: parsedBody.name},
-                "id": {S: parsedBody.id}
-            }
-        }, function(err, data) {
+exports.handler = async () => {
+    const getData = new Promise((resolve, reject) => {
+        dynamoDB.scan({ TableName: "languageAppIn" }, (err, data) => {
             if (err) {
-                console.log('Error putting item into dynamodb failed: '+err);
-            }
-            else {
-                getData(params)
-            }
-        });
-    }
-}
+                reject(err);
+            } else {
+                const unmarshalledData = data.Items.map((el) => {
+                    return AWS.DynamoDB.Converter.unmarshall(el);
+                });
+
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(unmarshalledData),
+                headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+                },
+            };
+
+            resolve(response);
+        }
+      });
+    });
+
+    return getData;
+};
