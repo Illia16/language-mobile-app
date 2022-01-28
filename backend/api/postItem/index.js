b995a7df39d72fcc49d05be4751af92633ad51b0
Illia16 <illia2143@gmail.com>
2022-01-27 21:37:57 -0500

    wip: backend post item

--- backend/api/postItem/index.js
+++ backend/api/postItem/index.js
@@ -1,42 +1,10 @@
 const AWS = require('aws-sdk')
 const dynamoDB = new AWS.DynamoDB({ region: 'us-east-1', apiVersion: '2012-08-10' })
+const { handler: getData } = require('../getItem/index.js')
 
-exports.handler = (event, context, callback) => {
-    const getData = (params) => {
-        dynamoDB.scan(params, (err, data) => {
-          if (err) {
-              callback(err)
-          } else {
-                const unmarshalledData = data.Items.map(el => {
-                    return AWS.DynamoDB.Converter.unmarshall(el)
-                })
-
-                const response = {
-                    "statusCode": 200,
-                    "body": JSON.stringify(unmarshalledData),
-                    "headers": {
-                        "Access-Control-Allow-Origin": "*",
-    	            "Access-Control-Allow-Credentials": true
-                    }
-
-                };
-
-                callback(null, response)
-          }
-        })
-    }
-
-    const params = {
-        TableName: 'languageAppIn'
-    }
-
-    if (event.httpMethod === 'GET') {
-            getData(params)
-    }
-
-    if (event.httpMethod === 'POST') {
+exports.handler = async (event) => {
+    const postData = new Promise((resolve, reject) => {
         const parsedBody = JSON.parse(event.body);
-
         dynamoDB.putItem({
             TableName: "languageAppIn",
             Item : {
@@ -44,14 +12,15 @@ exports.handler = (event, context, callback) => {
                 "moves": {N: parsedBody.moves},
                 "name": {S: parsedBody.name},
                 "id": {S: parsedBody.id}
-            }
-        }, function(err, data) {
+            },
+        }, (err) => {
             if (err) {
-                console.log('Error putting item into dynamodb failed: '+err);
+                reject(err);
+            } else {
+                resolve(getData({ TableName: "languageAppIn" }));
             }
-            else {
-                getData(params)
-            }
-        });
-    }
+        })
+    });
+
+    return postData;
 }
