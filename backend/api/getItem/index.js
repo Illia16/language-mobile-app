e89b797ba779280b3f8e1c993713f1274a6eff2d
Illia16 <illia2143@gmail.com>
2022-01-30 21:27:08 -0500

    backend: getItem Promise reworked to async await

--- backend/api/getItem/index.js
+++ backend/api/getItem/index.js
@@ -5,26 +5,23 @@ const dynamoDB = new AWS.DynamoDB({
 });
 
 exports.handler = async () => {
-    return new Promise((resolve, reject) => {
-        dynamoDB.scan({ TableName: "languageAppIn" }, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                const unmarshalledData = data.Items.map((el) => {
-                    return AWS.DynamoDB.Converter.unmarshall(el);
-                });
+    try {
+        const res = await dynamoDB.scan({ TableName: "languageAppIn" }).promise();
+        const unmarshalledData = res.Items.map((el) => {
+            return AWS.DynamoDB.Converter.unmarshall(el);
+        });
 
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify(unmarshalledData),
-                headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": true,
-                },
-            };
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(unmarshalledData),
+            headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+            },
+        };
 
-            resolve(response);
-        }
-      });
-    });
-};
+        return response;
+    } catch (er) {
+        return er;
+    }
+}
